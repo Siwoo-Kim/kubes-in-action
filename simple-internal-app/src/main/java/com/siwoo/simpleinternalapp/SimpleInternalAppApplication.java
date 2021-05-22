26d046b0006aeb680d7db8e7d5361e462179801b
kimsi <Siwoo.Kim@pointclickcare.com>
2021-05-22 13:33:38 -0400

    deployment object to update the apps

--- simple-internal-app/src/main/java/com/siwoo/simpleinternalapp/SimpleInternalAppApplication.java
+++ simple-internal-app/src/main/java/com/siwoo/simpleinternalapp/SimpleInternalAppApplication.java
@@ -1,11 +1,15 @@
 package com.siwoo.simpleinternalapp;
 
 import com.google.common.collect.ImmutableMap;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 @SpringBootApplication
 public class SimpleInternalAppApplication {
@@ -14,12 +18,25 @@ public class SimpleInternalAppApplication {
 		SpringApplication.run(SimpleInternalAppApplication.class, args);
 	}
 	
+	@Slf4j
 	@RestController
-	public class InternalController {
+	public static class InternalController {
+		private int reqCnt = 0;
 		
 		@GetMapping("/data")
 		public ResponseEntity<?> call() {
 			return ResponseEntity.ok(ImmutableMap.of("key", "1"));
 		}
+
+		@GetMapping("/version")
+		public ResponseEntity<?> version(HttpServletRequest req) {
+			log.info("Received request from {}", req.getRemoteAddr());
+			log.info("Smth changed");
+			if (++reqCnt >= 5) {
+				return ResponseEntity.status(500).body(ImmutableMap.of("error", "internal error"));
+			}
+			return ResponseEntity.ok(ImmutableMap.of("version", System.getenv("APP_VERSION")));
+		}
+
 	}
 }
